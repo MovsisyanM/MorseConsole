9b3485ac4152697d30bcfa96e1ef60ca6aa03846
MovsisyanM <61076650+MovsisyanM@users.noreply.github.com>
2020-02-15 02:25:46 -0800

    Initial Commit

---
+++ Morse/Morse.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse
+{
+    public static class Morse
+    {
+        private enum alphabet
+        {
+            Zero,One,Two,Three,Four,Five,Six,Seven,Eight,Nine,
+            A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+        
+        }
+
+        static string toMorse(this char c)
+        {
+            switch (c)
+            {
+                case '0':
+                    return "-----";
+                case '1':
+                    return ".----";
+                case '2':
+                    return "..---";
+                case '3':
+                    return "...--";
+                case '4':
+                    return "....-";
+                case '5':
+                    return ".....";
+                case '6':
+                    return "-....";
+                case '7':
+                    return "--...";
+                case '8':
+                    return "---..";
+                case '9':
+                    return "----.";
+                case 'A':
+                    return ".-";
+                case 'B':
+                    return "-...";
+                case 'C':
+                    return "-.-.";
+                case 'D':
+                    return "-..";
+                case 'E':
+                    return ".";
+                case 'F':
+                    return "..-.";
+                case 'G':
+                    return "--.";
+                case 'H':
+                    return "....";
+                case 'I':
+                    return "..";
+                case 'J':
+                    return ".---";
+                case 'K':
+                    return "-.-";
+                case 'L':
+                    return ".-..";
+                case 'M':
+                    return "--";
+                case 'N':
+                    return "-.";
+                case 'O':
+                    return "---";
+                case 'P':
+                    return ".--.";
+                case 'Q':
+                    return "--.-";
+                case 'R':
+                    return ".-.";
+                case 'S':
+                    return "...";
+                case 'T':
+                    return "-";
+                case 'U':
+                    return "..-";
+                case 'V':
+                    return "...-";
+                case 'W':
+                    return ".--";
+                case 'X':
+                    return "-..-";
+                case 'Y':
+                    return "-.--";
+                case 'Z':
+                    return "--..";
+                default:
+                    return "-----";
+            }
+        }
+
+        public readonly static List<char> convertableChars = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        
+
+        public static List<string> ToMorse(this string Str)
+        {
+            List<char> output = new List<char>();
+            foreach (char c in Str.ToUpper().ToCharArray())
+            {
+                if (convertableChars.Contains<char>(c))
+                {
+                    output.Add(c);
+                }
+            }
+
+            List<string> morse = new List<string>();
+            foreach (char c in output)
+            {
+                morse.Add(c.toMorse());
+            }
+            return morse;
+        }
+    }
+}
